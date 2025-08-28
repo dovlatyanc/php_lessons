d980103ffd4fca15d82f054a78c9012e5a43a025
dovlatyanc <dovlatyanc.ru@gmail.com>
2025-08-28 20:49:53 +0300

    сделал 4 задачу

--- tasks/task4.php
+++ tasks/task4.php
@@ -1,15 +1,18 @@
 <?php
-echo "ЗАДАЧА 3!<br>";
+echo "ЗАДАЧА 4<br>";
 if(!isset($_POST['submit'])){
 $num = $_POST['num'];
 $result = 0;
 $sum  = 0;
 $str = (string) $num;
         for ($i = 0; $i < strlen($str); $i++) {
-            
-       
+            $sum+=$str[$i];
         }
-    echo "Среднее арифметическое: $result<br>";
+        $result =  $sum/strlen($str);
+        echo "Число: $num<br>";
+        echo "Сумма цифр: $sum<br>";
+        echo "Количество цифр: " . strlen($str) . "<br>";
+        echo "Среднее арифметическое: " . number_format($result, 2) . "<br>";
 
 }else{
     echo "Кнопка не нажата!<br>";
