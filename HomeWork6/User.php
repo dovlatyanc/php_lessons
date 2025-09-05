cd95ef26f0cb7896a0ecc0934848bf7b0c15d782
dovlatyanc <dovlatyanc.ru@gmail.com>
2025-09-05 13:39:21 +0300

    feat: add class User

---
+++ HomeWork6/User.php
@@ -0,0 +1,24 @@
+<?php
+
+class User{
+
+   private int $id;
+   private string $firstName;
+   private string $lastName;
+   private string $email;
+   private string $password;
+ 
+   public function __construct($id, $firstName, $lastName, $email, $password)
+   {
+        $this->id =$id;
+        $this->firstName =$firstName;
+        $this->lastName =$lastName;
+        $this->email =$email;
+        $this->password =$password;
+   }
+     public function __toString()
+    {
+        return "Пользователь #{$this->id}: {$this->firstName}
+         {$this->lastName} ({$this->email})";
+    }
+}
