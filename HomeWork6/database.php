23d2f028dd9fa1f170acac084bcc331cc5f514f2
dovlatyanc <dovlatyanc.ru@gmail.com>
2025-09-05 13:52:30 +0300

    feat: add method initDatabase

--- HomeWork6/database.php
+++ HomeWork6/database.php
@@ -3,17 +3,27 @@
 class Database {
     private PDO $pdo;
 
-
-
     public function __construct(){
         try {
             
             $this->pdo = new PDO('sqlite:users.sqlite');
-            
+             
             // Устанавливаем режим ошибок - исключения
             $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         } catch (PDOException $e) {
             die("Ошибка подключения к базе данных: " . $e->getMessage());
         }
     }
+    //создаем БД если ее нет
+    public function initDatabase() {
+    $sql = "CREATE TABLE IF NOT EXISTS users (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        first_name TEXT NOT NULL,
+        last_name TEXT NOT NULL,
+        email TEXT UNIQUE NOT NULL,
+        password TEXT NOT NULL
+    )";
+    
+    $this->pdo->exec($sql);
+}
 }
