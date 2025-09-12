9c10ac737b08ff360068af93eb7b883d254e6ab6
dovlatyanc <dovlatyanc.ru@gmail.com>
2025-09-12 18:13:51 +0300

    распечатываем инфу о случайном пользователе

--- HomeWork7/index.php
+++ HomeWork7/index.php
@@ -1,20 +1,23 @@
 <?php
-
+require_once "randFiling.php";
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./index.css">
-    <title>HW7</title>
-</head>
-<body>
-    <h1>Случайный пользователь:</h1>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="./index.css">
+        <title>HW7</title>
+    </head>
+    <body>
+        <h1>Случайный пользователь:</h1>
 
-<div class="user-info">
-    
-</div>
-</body>
+        <div class="user-info">
+             <div> 
+                <?php echo $user;
+                 ?>
+            </div>  
+        </div>
+    </body>
 </html>
