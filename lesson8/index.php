fccd69221ca6a522aece6f347123c6556a8a7670
dovlatyanc <dovlatyanc.ru@gmail.com>
2025-09-12 18:36:04 +0300

    fix: чуть отрефакторил

--- lesson8/index.php
+++ lesson8/index.php
@@ -11,10 +11,8 @@
         <h1>Все задачи</h1>
 
         <?php 
-            $pdo =new PDO('mysql:host=localhost;dbname=pv311;charset=utf8mb4','root','',[
-                PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION,
-                PDO::ATTR_DEFAULT_FETCH_MODE=>PDO::FETCH_ASSOC,
-            ]);
+            require_once 'db.php';
+            $pdo = getPDO();
             $stmt =$pdo->query('select *  from tasks');
 
             echo '<div class = "grid">';
