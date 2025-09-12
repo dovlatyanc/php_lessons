ad568227fedfab3580d8b8d5fb4681ff6bfa48e3
dovlatyanc <dovlatyanc.ru@gmail.com>
2025-09-12 18:56:39 +0300

    feat: реализована страница обновления задачи

---
+++ lesson8/update.php
@@ -0,0 +1,28 @@
+<?php
+require_once 'db.php';
+
+if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
+    header('Location: index.php');
+    exit;
+}
+
+//Получение данных из формы
+$id = $_POST['id'] ?? null;
+$name = trim($_POST['name'] ?? '');
+$due = $_POST['due'] ?? '';
+$priority = (int)($_POST['prioryty'] ?? 1);
+$description = trim($_POST['description'] ?? '');
+
+
+if (!$id || !$name || $priority < 1 || $priority > 5) {
+    die('Некорректные данные.');
+}
+
+$pdo = getPDO();
+
+$stmt = $pdo->prepare('UPDATE tasks SET name = ?, due = ?, prioryty = ?, description = ? WHERE id = ?');
+
+$stmt->execute([$name, $due, $priority, $description, $id]);//безопасное заполнение
+
+header('Location: index.php');//После успешного обновления возвращает на главную страницу
+exit;
